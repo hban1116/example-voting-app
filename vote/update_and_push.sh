4bdbee56f1add84259a4776a4e0f6ea0bc858d65
hban1116 <harshitbansal1116@gmail.com>
2024-08-01 18:12:22 +0530

    Update update_and_push.sh

--- vote/update_and_push.sh
+++ vote/update_and_push.sh
@@ -19,7 +19,7 @@ git add k8s-specifications/vote-deployment.yaml
 # Commit the changes
 git commit -m "Update voting-app image to gcr.io/$PROJECT_ID/voting-app:$COMMIT_SHA"
 
-git remote set-url origin https://final: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@github.com/hban1116/example-voting-app.git
+git remote set-url origin https://hban1116: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@github.com/hban1116/example-voting-app.git
 
 # Push the changes
 git push 
